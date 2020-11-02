e8e8f8fc37e786c52f2b379a402d6547ce6c0859
Haryel Ramalho <haryel.dev@gmail.com>
2020-11-02 14:13:01 +0000

    Updates

---
+++ precache-manifest.0c893f37e4f00e5ab199e95ab5eac59c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8a912b607b91d489d13e9ee3603423",
+    "url": "/gofinances/index.html"
+  },
+  {
+    "revision": "72272308dd7f0dcb2340",
+    "url": "/gofinances/static/js/2.dfc86167.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gofinances/static/js/2.dfc86167.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d371f879f0a1f63117f8",
+    "url": "/gofinances/static/js/main.235c8d8e.chunk.js"
+  },
+  {
+    "revision": "837571d0364c3f78df64",
+    "url": "/gofinances/static/js/runtime-main.d9bd4327.js"
+  },
+  {
+    "revision": "78a343ae8fc9abcf44feb04b5656f5fc",
+    "url": "/gofinances/static/media/alert.78a343ae.svg"
+  },
+  {
+    "revision": "df4567f063bf4458db532e5ac63df30a",
+    "url": "/gofinances/static/media/income.df4567f0.svg"
+  },
+  {
+    "revision": "ce269cac7c70f224e69b361665e6d3e0",
+    "url": "/gofinances/static/media/logo.ce269cac.svg"
+  },
+  {
+    "revision": "bb31a5f33ccfe334fa7c72e294ba2984",
+    "url": "/gofinances/static/media/outcome.bb31a5f3.svg"
+  },
+  {
+    "revision": "7f99c5ac38306dc0adf301bf383f882f",
+    "url": "/gofinances/static/media/total.7f99c5ac.svg"
+  }
+]);
